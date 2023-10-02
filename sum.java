77b14479d870872cbe02dad8d268c6a463b63f74
Atharva More <atharvamore062002@gmail.com>
2023-10-02 15:07:45 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(string[] args){
         int a=10;
         int b=12;
-        system.out.print(a+b);
+        system.out.print("the sum is "+(a+b));
     }
 }
